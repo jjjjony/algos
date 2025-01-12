4e24dc640194a0c7632e04729d5b68d95fe3100a
johnny <74356539+jjawny@users.noreply.github.com>
2025-01-12 18:14:49 +1000

    was in c# brain mode

--- twoSum.ts
+++ twoSum.ts
@@ -5,19 +5,19 @@ function twoSum(nums: number[], target: number): number[] {
   // 1. Create a hashmap - we always lookup a nums item, so keys = nums item
   //    <K = nums item, V = item idx>
   //    IMRPOVEMENT: Skip the 1st loop (never finds a match)
-  var hashmap = new Map<number, number>([[nums[0], 0]]);
+  const hashmap = new Map<number, number>([[nums[0], 0]]);
 
   // 2. O(n) over nums, skipping the 1st loop (n - 1)
   for (let idx = 1; idx < nums.length; idx++) {
     // 3. Diff to get the remainder to find
-    var num = nums[idx];
-    var remainder = target - num;
+    const num = nums[idx];
+    const remainder = target - num;
     console.log(`${num} + ${remainder} = ${target}`);
 
     // 4. O(1) search the hashmap
     //    Found? Success!
     //    Not found? cache and repeat...
-    var remainderIdx = hashmap.get(remainder);
+    const remainderIdx = hashmap.get(remainder);
     if (remainderIdx !== undefined) return [remainderIdx, idx];
     hashmap.set(num, idx);
   }
