eeb4cc94d745e703539de1abfdab4a99eb170e7e
johnny <74356539+jjawny@users.noreply.github.com>
2025-01-15 21:50:44 +1000

    day 4

---
+++ missingNumber.ts
@@ -0,0 +1,35 @@
+/**
+ * BIG-O: O(n)
+ */
+function missingNumber(nums: number[]): number {
+  const n = nums.length;
+
+  // SOLUTION 1 = O(n) + O(n) = O(n)
+  //  In both solutions we need something to track nums/find the missing num
+  //  Since we know n upfront, create an array (O(1)) w empty slots (the indexes are useful)
+  //  Offset by 1 since we don't count 0
+  // const foundNums = new Array(n + 1);
+  // for (const num of nums) foundNums[num] = true;
+  // for (let i = 1; i <= n; i++) {
+  //     const isMissing = !foundNums[i];
+  //     if (isMissing) return i;
+  // }
+
+  // SOLUTION 2 = O(n)
+  //  Remove one of the O(n) traversals by using the sum (which we know upfront)
+  let sum = (n * (n + 1)) / 2;
+  for (const num of nums) sum -= num;
+  return sum;
+
+  return 0;
+}
+
+/**
+ * SUBMISSION: Minified v w no thoughts 💭
+ */
+function missingNumber_SUBMISSION(nums: number[]): number {
+  const n = nums.length;
+  let sum = (n * (n + 1)) / 2;
+  for (const num of nums) sum -= num;
+  return sum;
+}
